f0c94a41bd890b5e8b09518a573f2b31b254c687
JUAN manuel calleros <116044083+manuelcalso@users.noreply.github.com>
2023-10-11 17:39:53 -0600

    se agrega musica

--- main.js
+++ main.js
@@ -34,6 +34,35 @@ const getData = async () => {
       <div>
         <h1>Hello Cat!</h1>
         <img src=" ${baseUrl}${data.url}" />
+        <div id="reproductor">
+          <iframe
+            width="100%"
+            height="166"
+            scrolling="no"
+            frameborder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1637329092&color=%2300ff18&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+          ></iframe>
+          <div
+            style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"
+          >
+            <a
+              href="https://soundcloud.com/calibresignature"
+              title="Calibre"
+              target="_blank"
+              style="color: #cccccc; text-decoration: none;"
+              >Calibre</a
+            >
+            ·
+            <a
+              href="https://soundcloud.com/calibresignature/more-fool-me"
+              title="More Fool Me"
+              target="_blank"
+              style="color: #cccccc; text-decoration: none;"
+              >More Fool Me</a
+            >
+          </div>
+        </div>
       </div>
     `
   } catch (error) {
